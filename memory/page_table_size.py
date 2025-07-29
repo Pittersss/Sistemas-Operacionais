876b883eb99481193c2d5685ebf7357069e8bd90
Pedro Henrique Malaquias da Silva <pedromalaquias@MacBook-Pro-de-Pedro.local>
2025-07-29 14:58:42 -0300

    feat:calc page table case 1

---
+++ memory/page_table_size.py
@@ -0,0 +1,37 @@
+import math
+
+def paging_translation(address_acess: int, address_space: int, physical_address_space: int, page_size: int):
+    address_acess_bin = bin(address_acess)[2:] #without prefix 0b
+    address_size = math.log2(address_space)
+    pages_quantity = address_space / page_size
+    page_frame_quantity = physical_address_space / page_size
+
+    page_table = generate_table(address_acess, physical_address_space ,page_size)
+
+    while len(address_acess_bin) != address_size:
+        address_acess_bin = "0" + address_acess_bin
+    
+
+
+    return type(address_acess_bin)
+
+def generate_table(address_space: int, physical_address_space: int ,page_size: int):
+    number_of_pages = address_space/page_size
+    number_of_pages_frames = physical_address_space/page_size
+
+
+
+
+def calc_page_table_size(address_space: int, page_size: int):
+    number_of_pages = address_space/page_size
+    number_of_pages_frames = number_of_pages #Assumindo
+    page_table_address = number_of_pages_frames
+    page_table_address_size = math.log2(page_table_address)
+    resul = page_table_address * page_table_address_size
+    return resul
+
+
+def calc_page_table_size_adress_size(adress_size: int):
+    return
+
+print(calc_page_table_size(64, 16))
