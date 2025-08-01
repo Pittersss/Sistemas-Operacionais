a0292f9e099292e721cc1c75e4bccb84f4afa253
Pedro Henrique Malaquias da Silva <pedromalaquias@MacBook-Pro-de-Pedro.local>
2025-08-01 14:57:56 -0300

    fix: generate paging table inconsistencies

--- memory/page_table_size.py
+++ memory/page_table_size.py
@@ -2,17 +2,18 @@ import math
 from random import randint
 
 def paging_translation(address_acess: int, address_space: int, physical_address_space: int, page_size: int):
-    address_acess_bin = bin(address_acess)[2:] #without prefix 0b
+    address_acess_bin = bin(address_acess)[2:] #without prefix 0b 
     address_size = math.log2(address_space)
-    pages_quantity = address_space / page_size
-    page_frame_quantity = physical_address_space / page_size
+    pages_quantity = address_space / page_size 
 
     page_table = generate_table(address_acess, physical_address_space ,page_size)
 
-    while len(address_acess_bin) != address_size:
-        address_acess_bin = "0" + address_acess_bin
+    size_dif = (len(address_acess_bin) - int(address_size))
+    address_acess_bin = ("0" * size_dif) + address_acess_bin
+    page_id = int(address_acess_bin[:int(math.log2(pages_quantity)) + 1], 2)
+    page_frame_id = search_in_page_table(page_id, page_size, page_table)
 
-    return type(address_acess_bin)
+    return page_frame_id
 
 def generate_table(address_space: int, physical_address_space: int ,page_size: int):
     number_of_pages = address_space/page_size
@@ -30,6 +31,23 @@ def generate_table(address_space: int, physical_address_space: int ,page_size: i
                 page_table.append(("0" * int(size_dif)) + bin(elem)[2:])
             else:
                 page_table.append(bin(elem)[2:])
+    
+    resul = "".join(page_table)
+
+    show_page_table_structured(resul, int(number_of_pages_frames))
+    
+    return resul
+
+
+def search_in_page_table(page_id: int, page_size: int ,page_table):
+    start_address = page_id * page_size
+    return int(page_table[start_address: (start_address + page_size)], 2)
+
+def show_page_table_structured(page_table, pages_frames_quantity : int):
+    print("Paging Table:\n")
+    for i in range(0, int(len(page_table) / pages_frames_quantity)):
+        start_pos = i * pages_frames_quantity
+        print(str(i) + " | " + str(int(page_table[ start_pos : start_pos + pages_frames_quantity], 2)) + "\n--------------------")
 
 
 
